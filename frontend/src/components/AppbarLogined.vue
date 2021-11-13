a7b946d4f4dea34d609c0400f6402f16c73a4fe9
정희상 <jerry400310@gmail.com>
2021-11-14 03:29:55 +0900

    dialog를 이용한 관심종목 설정 중간단계

--- frontend/src/components/AppbarLogined.vue
+++ frontend/src/components/AppbarLogined.vue
@@ -70,6 +70,80 @@
           ></v-divider>
         </template>
       </v-list>
+
+      <!-- dialog 이용하여 관심종목 설정-->
+      <div class="text-center">
+        <v-dialog v-model="dialog" width="900">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+              Click Me
+            </v-btn>
+          </template>
+
+          <v-card>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <p
+                    style="color: #64ffda"
+                    class="text-h4 text-center font-weight-black mb-0"
+                  >
+                    관심 종목 설정
+                  </p>
+                </v-col>
+              </v-row>
+              <v-row justify="end">
+                <v-col cols="8">
+                  <v-text-field
+                    v-model="magnify"
+                    solo
+                    dense
+                    label="종목"
+                    flat
+                    hide-details="auto"
+                    outlined
+                    style="border-radius: 15px"
+                  >
+                    <v-icon slot="append" color="black" large
+                      >mdi-magnify</v-icon
+                    >
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <template v-for="(chunk, index) in chunkStocks">
+                  <v-container v-if="page == index + 1" :key="index">
+                    <v-row v-for="(a, i) in 5" :key="i" justify="space-around">
+                      <v-col
+                        v-for="(ele, idx) in chunk.slice(i * 5, (i + 1) * 5)"
+                        :key="idx"
+                      >
+                        <v-checkbox
+                          v-model="selected"
+                          :label="ele"
+                          :value="ele"
+                        >
+                        </v-checkbox>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </template>
+              </v-row>
+              <v-row justify="center">
+                <v-pagination
+                  v-model="page"
+                  :length="pages"
+                  :total-visible="7"
+                  circle
+                ></v-pagination>
+              </v-row>
+              <v-row justify="end">
+                <v-btn text @click="dialog = false">확인</v-btn>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-dialog>
+      </div>
     </v-menu>
   </v-app-bar>
 </template>
@@ -79,6 +153,7 @@ export default {
   name: "AppbarLogined",
   data() {
     return {
+      dialog: false,
       items: [
         { content: "삼성전자 리포트가 업로드 되었습니다.", period: 3 },
         { content: "삼성전자 리포트가 업로드 되었습니다.", period: 2 },
@@ -96,9 +171,37 @@ export default {
         { content: "마이페이지", to: "/like-report" },
         { content: "로그아웃", to: "/logout" },
       ],
+      stocks: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+        39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+      ],
+      magnify: "",
+      page: 1,
+      selected: [],
+      stockscount: "",
     };
   },
-  methods: {},
+  computed: {
+    chunkStocks() {
+      let arr = [];
+      this.stocks.forEach((value, index) => {
+        if (index % 25 == 0) arr.push(this.stocks.slice(index, index + 25));
+      });
+      return arr;
+    },
+    pages() {
+      return Math.ceil(this.stockscount / 25);
+    },
+  },
+  created() {
+    this.initPage();
+  },
+  methods: {
+    initPage: function () {
+      this.stockscount = this.stocks.length;
+    },
+  },
 };
 </script>
 
