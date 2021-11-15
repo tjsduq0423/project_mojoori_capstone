61e6794674813f339c5bb8d126de05de07af8a48
Joon ho <tjsduq0423@uos.ac.kr>
2021-11-16 00:14:13 +0900

    배포

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "ec2-13-125-115-211.ap-northeast-2.compute.amazonaws.com",
   withCredentials: true,
 });
 
