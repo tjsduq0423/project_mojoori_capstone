f7aa5198f05f28fb8d891ae70ae03357c831493c
Joon ho <tjsduq0423@uos.ac.kr>
2021-12-13 16:44:51 +0900

    알람삭제후 최종

--- frontend/src/components/Appbar.vue
+++ frontend/src/components/Appbar.vue
@@ -17,28 +17,6 @@
 
     <v-spacer />
 
-    <!-- 알림 목록 -->
-    <v-menu v-if="this.$store.state.auth.auth" offset-y>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn large icon class="mx-1" v-bind="attrs" v-on="on">
-          <v-icon>mdi-bell-outline</v-icon>
-        </v-btn>
-      </template>
-      <v-list class="py-0">
-        <template v-for="(item, index) in items">
-          <v-list-item :key="index">
-            <v-list-item-title
-              >{{ item.content }} {{ item.period }}일전
-            </v-list-item-title>
-          </v-list-item>
-          <v-divider
-            v-if="index < items.length - 1"
-            :key="`${index} - divider`"
-          ></v-divider>
-        </template>
-      </v-list>
-    </v-menu>
-
     <!-- 게시판 버튼 -->
     <v-tooltip bottom color="blue darken-1">
       <template v-slot:activator="{ on, attrs }">
@@ -319,7 +297,6 @@ export default {
   name: "Appbar",
   data() {
     return {
-      items: null,
       dialog: false,
       dialog2: false,
       magnify_stock: "",
@@ -346,16 +323,7 @@ export default {
       return Math.ceil(this.industriescount / 25);
     },
   },
-  created() {
-    this.items = [
-      { content: "삼성전자 리포트가 업로드 되었습니다.", period: 3 },
-      { content: "삼성전자 리포트가 업로드 되었습니다.", period: 2 },
-      { content: "삼성전자 리포트가 업로드 되었습니다.", period: 1 },
-      { content: "삼성전자 리포트가 업로드 되었습니다.", period: 1 },
-      { content: "삼성전자 리포트가 업로드 되었습니다.", period: 2 },
-      { content: "삼성전자 리포트가 업로드 되었습니다.", period: 3 },
-    ];
-  },
+  created() {},
   methods: {
     async logout() {
       try {
