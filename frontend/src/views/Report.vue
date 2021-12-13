0b47cc9af600d5e4d1d50de6593969284e3a2f39
Joon ho <tjsduq0423@uos.ac.kr>
2021-12-13 23:56:08 +0900

    ...

--- frontend/src/views/Report.vue
+++ frontend/src/views/Report.vue
@@ -59,7 +59,7 @@ export default {
   computed: {
     ...mapState("auth", ["userId"]),
   },
-  created() {
+  mounted() {
     this.callInterest();
     this.callData();
     this.callPopularStock();
